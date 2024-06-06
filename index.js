48d1a6bba393ed4b6bdddbba796e8378e442af7b
Bruno Marinho <brunohenrique94@outlook.com>
2024-06-06 14:59:03 -0300

    feat: implementa contador individual de bolas sorteadas e exibe no console

--- index.js
+++ index.js
@@ -4,11 +4,29 @@ const bolas = [
     "amarelo", "amarelo", "amarelo",
     "vermelho", "vermelho", "vermelho", "vermelho", "vermelho"]
 
+let contadorVerde = 0
+let contadorAzul = 0
+let contadorAmarelo = 0
+let contadorVermelho = 0
+
 for(let i = 1; i <= 1000; i++){
     let linha = ""
     for (let j = 0; j < 4; j++){    
         let indice = Math.floor(Math.random() * bolas.length) // Obtém um número aleatório de 0 a 10
+        if (bolas[indice] === "verde"){
+            contadorVerde++
+        } else if (bolas[indice] === "azul"){
+            contadorAzul++
+        } else if (bolas[indice] === "amarelo"){
+            contadorAmarelo++
+        } else if (bolas[indice] === "vermelho"){
+            contadorVermelho++
+        }
         linha = linha + bolas[indice] + ","
     }
-    console.log(linha.slice(0,-1))
 }
+
+console.log("verde: " + contadorVerde)
+console.log("azul: " + contadorAzul)
+console.log("amarelo: " + contadorAmarelo)
+console.log("vermelho: " + contadorVermelho)
